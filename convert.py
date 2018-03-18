006b3b3190630a35a40d0d54e94070226c9c04ff
nadysaptra <saputranady@gmail.com>
2018-03-18 07:17:47 +0700

    video example

--- convert.py
+++ convert.py
@@ -2,9 +2,14 @@ from os import listdir
 from os.path import isfile, join
 import os
 import subprocess
-mypath    = '/the-path-to-video/'
+cur_dir     = os.path.dirname(os.path.realpath(__file__))
+mypath      = '%s/files' % cur_dir
+if not os.path.exists('%s/convert' % mypath ):
+    os.makedirs('%s/convert' % mypath)
 onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 quality   = 24
 for f in onlyfiles:
     print('files %s' % (f))
-    os.system('ffmpeg -i /the-path-to-video/%s -vcodec libx264 -crf %s /the-path-to-results/convert/%s' % (f, quality, f))
+    path        = '%s/%s' % (mypath, f)
+    result_path = '%s/convert/%s' % (mypath, f)
+    os.system('ffmpeg -i %s -vcodec libx264 -crf %s %s' % (path, quality, result_path))
